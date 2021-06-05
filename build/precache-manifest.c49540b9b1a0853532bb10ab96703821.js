dce59692bfbd4e3153f898dd692e87a6c294675a
shuhia <shuhia.on@gmail.com>
2021-06-06 01:10:37 +0200

    installed dependencies

---
+++ build/precache-manifest.c49540b9b1a0853532bb10ab96703821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1df1b4edb0ea09198ee9082e0ef3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7089982a4c413b3b45a3",
+    "url": "/static/css/main.474ddbc6.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7089982a4c413b3b45a3",
+    "url": "/static/js/main.3a6b6a84.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
